7152fa18f173ad172259c0654ea33a88db77c471
Jaronce Dutil <jaronce.d@gmail.com>
2019-11-19 17:22:12 -0500

    updated the books controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,13 +2,34 @@ class BookingsController < ApplicationController
 #CREATE-----------------------------------------
   def new
     @booking = Booking.new
+    @product = Product.find(params[:product_id])
   end
 
   def create
-
+    @booking = Booking.new(booking_params)
+    @product = Product.find(params[:product_id])
+    @booking.product = @product
+    @booking.user = current_user
+    if @booking.save
+      flash[:notice] = "Thank you for booking me!"
+      redirect_to user_path(current_user)
+    else
+      render :new
+    end
   end
 #UPDATE STATUS-----------------------------------
-  def update
+  def edit
+    @booking = Booking.find(params[:booking_id])
+  end
 
+  def update
+    @booking = Booking.find(params[:booking_id])
+
+  end
+
+#EXTRA-------------------------------------------
+  private
+  def booking_params
+    params.require(:booking).permit(:status, :startdate, :enddate, :product_id)
   end
 end

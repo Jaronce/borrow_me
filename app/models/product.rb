fad7d272bc74bb1d0e76742020c35591c5475690
Jung Yu <hjyu1408@gmail.com>
2019-11-19 15:26:04 -0500

    update again

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :user
-  has_many :bookingsbo
+  has_many :bookings, dependent: :destroy
   has_many :product_skills, dependent: :destroy
 
   mount_uploader :photo, PhotoUploader

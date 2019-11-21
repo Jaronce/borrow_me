60e7951f5d8bcb7d9cb1698656f9a9a3c22879c1
Jaronce Dutil <jaronce.d@gmail.com>
2019-11-21 17:25:17 -0500

    changed some booking stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
   def show
+    user = current_user
+    @my_products = user.products
+
+    @my_bookings = Booking.joins(:product).where(products: { user: user })
+
+    @pending_bookings = @my_bookings.select { |b| b.status == "Pending" }
+    @confirmed_bookings = @my_bookings.select { |b| b.status == "Confirmed" }
   end
 end
